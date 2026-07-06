e3e000ef828f8849bc5af59fee424fd653cb749f
Rogatnev Sergey <rogatnev.sergey@gmail.com>
2026-07-06 21:21:01 +0700

    update deps

---
+++ Client/Aggregation/MovingSumAggregation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarbonApi.Client.Aggregation
+{
+    public sealed class MovingSumAggregation : IAggregation
+    {
+        private readonly string period;
+
+        public MovingSumAggregation(string period)
+        {
+            if (period == null)
+            {
+                throw new ArgumentNullException(nameof(period));
+            }
+
+            this.period = period;
+        }
+
+        public string Apply(string path)
+        {
+            return $"movingSum({path},\"{this.period}\")";
+        }
+    }
+}
